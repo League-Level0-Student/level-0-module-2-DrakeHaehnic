338e68420e8ea210a97f9f08c74e1f6891ecd16f
root <root@ca92a6505a56>
2020-09-29 00:22:13 +0000

    mygrjytdbjydvbyt

---
+++ src/_03_string_conversion/_4_voting_booth/thing.java
@@ -0,0 +1,40 @@
+package _03_string_conversion._4_voting_booth;
+
+import javax.swing.JOptionPane;
+
+public class thing {
+public static void main(String[] args) {
+String ht = JOptionPane.showInputDialog("wat bee yer age");
+int h = Integer.parseInt(ht);
+if (h>18) {
+	JOptionPane.showInputDialog("hoo do ye wish te be prezidint");
+}
+else {
+	JOptionPane.showMessageDialog(null, "no one cars wat ye theenk");
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
